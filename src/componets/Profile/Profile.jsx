940fe04eae019f7b75f80283b85d393cb48ff079
RotrixxLOL <ncusnir43@gmail.com>
2022-10-20 20:59:13 +0200

    file & folder structure

---
+++ src/componets/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => {
+  console.log('Profile');
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Ola</p>
+    </div>
+  );
+};
+
+export default Profile;
