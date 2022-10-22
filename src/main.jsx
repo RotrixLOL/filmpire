0ee49a7fe66d82c2b2b1a98cffe22a4aa30ce672
RotrixxLOL <ncusnir43@gmail.com>
2022-10-22 13:03:25 +0200

    update .env.exmple & update index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './app/store';
+
+const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
