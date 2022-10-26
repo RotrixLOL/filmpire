a49e279fa9edc498c86b52a7b1f69377131cf5a2
RotrixxLOL <ncusnir43@gmail.com>
2022-10-26 22:22:50 +0200

    replace /approved route to redirect to /

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { CssBaseline } from '@mui/material';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import useStyles from './styles';
 import useAlan from './components/Alan';
@@ -23,7 +23,7 @@ function App() {
           <Route exact path="/movie/:id" element={<MovieInformation />} />
           <Route exact path="/actors/:id" element={<Actors />} />
           <Route exact path="/" element={<Movies />} />
-          <Route exact path="/approved" element={<Movies />} />
+          <Route exact path="/approved" element={<Navigate to="/" replace />} />
           <Route exact path="/profile/:id" element={<Profile />} />
         </Routes>
       </main>
